0351b17fef7f2d974ff667385ed85b149fe48b9b
polina shibanova <pshibanova00@mail.ru>
2021-09-24 22:00:04 +0300

    feat: rendered category component

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { TestData } from '../data/testData';
+import { Category } from '../model/Category';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,7 @@ import { Injectable } from '@angular/core';
 export class DataHandlerService {
 
   constructor() { }
+  getCategories(): Category[]{
+    return TestData.categories;
+  }
 }
--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/model/Category';
+import { DataHandlerService } from 'src/app/service/data-handler.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[];
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
-    
+    this.categories = this.dataHandler.getCategories();
+    console.log(this.categories)
   }
 
 }
